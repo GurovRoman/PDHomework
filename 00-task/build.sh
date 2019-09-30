b73f5b1f1681ef9d33c510b5dc694d09dbeb485f
Студент КАиТП 5-11 <pd79511@calc.cod.phystech.edu>
2019-10-01 01:39:51 +0300

    Fix module loading

--- 00-task/build.sh
+++ 00-task/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+module load mpi/openmpi4-x86_64
+
 mpic++ -std=c++11 -g main.cpp -o main
--- 00-task/run.sh
+++ 00-task/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+module load mpi/openmpi4-x86_64
+
 mpirun main ${1:-100} 2> /dev/null
