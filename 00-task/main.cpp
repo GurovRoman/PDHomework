ed0d4c02fd02d2901b7710019243e7dd94d421b8
Студент КАиТП 5-11 <pd79511@calc.cod.phystech.edu>
2019-10-06 20:10:45 +0300

    Get rid of Scatter, Gather and Reduce

--- 00-task/main.cpp
+++ 00-task/main.cpp
@@ -7,6 +7,11 @@
 #include <ostream>
 
 
+int getDataSizeForRank(int rank, int world_size, int data_size) {
+    return data_size / world_size + (rank < data_size % world_size);
+}
+
+
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 
@@ -15,31 +20,37 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     int data_size = std::stoi(argv[1]);
+    int chunk_size = getDataSizeForRank(current_rank, world_size, data_size);
     std::vector<int> data;
 
     if (current_rank == 0) {
-        data.resize(data_size, 0);
+        data.resize(data_size);
         std::iota(data.begin(), data.end(), 0);
+        auto cur_data = data.data() + chunk_size;
+        for (int i = 1; i < world_size; ++i) {
+            auto size = getDataSizeForRank(i, world_size, data_size);
+            MPI_Send(cur_data, size, MPI_INT, i, 0, MPI_COMM_WORLD);
+            cur_data += size;
+        }
+    } else {
+        data.resize(chunk_size);
+        MPI_Recv(data.data(), chunk_size, MPI_INT, 0, 0, MPI_COMM_WORLD, nullptr);
     }
 
-    if (data_size % world_size) {
-        data_size += world_size - data_size % world_size;
-    }
+    long long sum = std::accumulate(data.begin(), data.begin() + chunk_size, 0);
 
-    data.resize(data_size, 0);
+    if (current_rank != 0) {
+        MPI_Send(&sum, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
+    } else {
+        std::vector<long long> partial_sums(world_size);
 
-    std::vector<int> chunk(data_size / world_size);
-    MPI_Scatter(data.data(), chunk.size(), MPI_INT, chunk.data(), chunk.size(), MPI_INT, 0, MPI_COMM_WORLD);
+        partial_sums[0] = sum;
+        for (int i = 1; i < world_size; ++i) {
+            MPI_Recv(partial_sums.data() + i, 1, MPI_LONG_LONG, i, 0, MPI_COMM_WORLD, nullptr);
+        }
+        
+        auto final_sum = std::accumulate(partial_sums.begin(), partial_sums.end(), 0);
 
-    long long sum = std::accumulate(chunk.begin(), chunk.end(), 0);
-
-    long long final_sum = 0;
-    MPI_Reduce(&sum, &final_sum, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    std::vector<long long> partial_sums(world_size);
-    MPI_Gather(&sum, 1, MPI_LONG_LONG, partial_sums.data(), 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
-
-    if (current_rank == 0) {
         std::cout << "Partial sums from nodes:\n";
         for (size_t i = 0; i < partial_sums.size(); ++i) {
             std::cout << "rank: " << i << ", sum: " << partial_sums[i] << '\n';
